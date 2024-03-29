b2159b7ade6665ebd1f5279346621a6a9fc79a2b
1kangyun1 <1kangyun1@gmail.com>
2024-03-29 16:18:28 -0400

    create form for creating tasks

---
+++ app/ui/dashboard/create-form.tsx
@@ -0,0 +1,134 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+import Link from 'next/link';
+import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+import { Button } from '@/app/ui/button';
+import { createTask } from '@/app/lib/actions';
+
+export default function Form() {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createTask, initialState);
+
+  return (
+    <form action={dispatch}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        {/* Task title */}
+        <div className="mb-4">
+          <label htmlFor="title" className="mb-2 block text-sm font-medium">
+            Enter a title
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                id="title"
+                name="title"
+                type="string"
+                placeholder="Enter a title"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-5 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="title-error"
+              />
+            </div>
+          </div>
+          <div id="title-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.title &&
+              state.errors.title.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+
+        {/* Task description */}
+        <div className="mb-4">
+          <label
+            htmlFor="description"
+            className="mb-2 block text-sm font-medium"
+          >
+            Enter a description
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                id="description"
+                name="description"
+                type="string"
+                placeholder="Enter a description"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-5 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="description-error"
+              />
+            </div>
+          </div>
+          <div id="description-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.description &&
+              state.errors.description.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+
+        {/* Task Status */}
+        <fieldset>
+          <legend className="mb-2 block text-sm font-medium">
+            Set the task status
+          </legend>
+          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+            <div className="flex gap-4">
+              <div className="flex items-center">
+                <input
+                  id="pending"
+                  name="status"
+                  type="radio"
+                  value="pending"
+                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
+                />
+                <label
+                  htmlFor="pending"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+                >
+                  Pending <ClockIcon className="h-4 w-4" />
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="finished"
+                  name="status"
+                  type="radio"
+                  value="finished"
+                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                />
+                <label
+                  htmlFor="finished"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+                >
+                  finished <CheckIcon className="h-4 w-4" />
+                </label>
+              </div>
+            </div>
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </fieldset>
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Create Task</Button>
+      </div>
+    </form>
+  );
+}
